e20d249c6564ca11b4c5d91ab767a89cff199e2e
mckenney5 <mckenneyadam@gmail.com>
2020-10-23 10:36:32 -0400

    Added features
    - Added a panic function to display a message then halt
    - Added a halt function that ignores interupts and halts
    - Added an init function for the kernel
    - Added a command loop function

--- src/kernel.c
+++ src/kernel.c
@@ -15,12 +15,49 @@
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
  
+ static void halt_and_catch_fire(){
+	 __asm__("cli\n.hang:\nhlt\njmp .hang\n.end:");
+ }
+ 
+static void panic(char* msg){
+	// unrecoverable error, throw a message then die
+	terminal_set_color(VGA_COLOR_WHITE, VGA_COLOR_RED);
+	printf("\nKERNEL: ");
+	printf(msg);
+	puts(" PANIC!");
 
+halt_and_catch_fire();
+}
+
+void sleep(unsigned int scale){
+	// poor mans sleep, not accurate AT ALL and I hope you like cooling fans, TODO use clock
+	unsigned long i = scale * 221720 * 10;
+	for( ; i != 0; i--)
+		__asm__("NOP");
+}
+
+void kernel_init(){
+// do something once
+	terminal_initialize();
+	printf("Welcome!\n");
+	return;
+}
+
+void kernel_loop(){
+// do this forever
+	char i=0;
+	while(1){
+	// write all chars to the screen
+		i++;
+		if(i != '\n' && i != '\v') putchar(i);
+		sleep(2);
+		break;
+	}
+	return;
+}
  
 void kernel_main(void){
-	/* Initialize terminal interface */
-	terminal_initialize();
- 
-	/* Newline support is left as an exercise. */
-	puts("Hello, World!");
+	kernel_init();
+	kernel_loop();
+	panic("No More Instructions");
 }
