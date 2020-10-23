2f9ecad51059b26f6e31ec07f3f221b59de03d42
mckenney5 <mckenneyadam@gmail.com>
2020-10-23 10:34:11 -0400

    Added scrolling, comments, and options for color

--- src/include/vga.h
+++ src/include/vga.h
@@ -5,6 +5,8 @@
 #include <stdint.h>
 
 #define TAB_SIZE 2
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
  
 /* Hardware text mode color constants. */
 enum vga_color {
@@ -34,59 +36,100 @@ static inline uint16_t vga_entry(unsigned char uc, uint8_t color){
 	return (uint16_t) uc | (uint16_t) color << 8;
 }
 
- 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
+
+static uint16_t screen_buff[VGA_WIDTH * VGA_HEIGHT] = {0};
  
 static size_t terminal_row;
 static size_t terminal_column;
 static uint8_t terminal_color;
 static uint16_t* terminal_buffer;
- 
-void terminal_initialize(void){
-	terminal_row = 0;
-	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
+
+void clear_screen(){
+// writes spaces onto the whole screen
+	
+	for (size_t y = 0; y < VGA_HEIGHT; y++){
+		for (size_t x = 0; x < VGA_WIDTH; x++){
 			const size_t index = y * VGA_WIDTH + x;
 			terminal_buffer[index] = vga_entry(' ', terminal_color);
 		}
 	}
+	// moves the printing head to 0,0
+	terminal_row = 0;
+	terminal_column = 0;
+}
+
+void copy_screen(){
+// copies what is on the screen to memory for future use
+	size_t i = 0;
+	for(i=0; i != VGA_WIDTH * VGA_HEIGHT; i++) // clear old data
+		screen_buff[i] = 0;
+
+	for(i=0; i != VGA_WIDTH * VGA_HEIGHT; i++) // copy
+		screen_buff[i] = terminal_buffer[i];
+	
+}
+
+static void scroll(){
+// scrolls the screen up one line
+	
+	copy_screen();
+	clear_screen();
+
+	// rewrite the screen but skip the first line
+	size_t i = 0, l=0;
+	for(i=VGA_WIDTH; i != VGA_WIDTH * VGA_HEIGHT; i++)
+		terminal_buffer[l++] = screen_buff[i];
+	
+	// move the printing head to the last line
+	terminal_row = VGA_HEIGHT-1;
+}
+
+void terminal_initialize(void){
+// sets up the screen colors and sets up the screen buffer
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_buffer = (uint16_t*) 0xB8000;
+	clear_screen();
 }
  
-void terminal_set_color(uint8_t color){
-	terminal_color = color;
+void terminal_set_color(enum vga_color fg, enum vga_color bg){
+	terminal_color = vga_entry_color(fg, bg);
 }
  
 static void terminal_put_entry_at(int c, uint8_t color, size_t x, size_t y){
+// writes to the screen, character - text-color - location
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
 }
 
-static void check_bounds(){
-	if (++terminal_column == VGA_WIDTH) {
-				terminal_column = 0;
-				if (++terminal_row == VGA_HEIGHT)
-					terminal_row = 0;
-			}
+static void inc_row(){
+// increases the row and checks if we hit the end of the screen
+	if(++terminal_row >= VGA_HEIGHT)
+		scroll();
 }
+
+static void inc_column(){
+// increases the column and checks if we hit the end of the screen
+	if (++terminal_column == VGA_WIDTH) { //if we wrote all the way right
+		terminal_column = 0; // move the printing head left
+		inc_row();
+	}
+}
+
  
 void terminal_putchar(int c){
+// external function to display text
 	switch((char) c){
 		case '\n':
 			terminal_column = 0;
-			terminal_row++;
-			check_bounds();
+			inc_row();
 			break;
 		case '\t':
 			terminal_column += TAB_SIZE;
-			check_bounds();
+			inc_column();
 			break;
 		case '\v':
-			terminal_row += TAB_SIZE;
-			check_bounds();
+			terminal_row += TAB_SIZE-1;
+			inc_row();
 			break;
 		case '\b':
 			if(terminal_column != 0) terminal_column--;
@@ -98,21 +141,12 @@ void terminal_putchar(int c){
 			break;
 		case '\r':
 			break;
+		case -1: //EOF
+			break;
 		default:
 			terminal_put_entry_at(c, terminal_color, terminal_column, terminal_row);
-			check_bounds();
+			inc_column();
 	}
 }
-
-/* 
-void terminal_write(const char* data, size_t size){
-	for (size_t i = 0; i < size; i++)
-		terminal_putchar(data[i]);
-}
- 
-void terminal_writestring(const char* data){
-	terminal_write(data, strlen(data));
-}
-*/
 
 #endif
